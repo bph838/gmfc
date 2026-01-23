aae47c9cf6008101174a4090ed09d5d242232d63
BP Hoops <benjaminpaulhooper@gmail.com>
2026-01-23 09:56:05 +0000

    Setup simple menu control and added styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,4 @@
 const { SITE_TITLE } = require("../js/constants");
 
-/*
-import { setupMenuCommands } from "./menu/menu";
-import { renderIndex } from "./pages/index";
-import { renderAlerts } from "./components/alerts";
-
-
-import data from '../data/pages/homepage.json';  
-console.log(data);
-setupMenuCommands(document,"page-home");
-renderIndex(data);  
-renderAlerts();  
-*/
- const contentarea = document.getElementById("contentarea");
-
-   const titleTextBar = document.createElement("div");      
-      titleTextBar.className="sectionheader";
-      contentarea.appendChild(titleTextBar);
+import { setupMenuCommands } from "../js/components/menu";
+setupMenuCommands("page-home");
