3ac0d09567cff0bb76797cacc3a8d03913743398
BP Hoops <benjaminpaulhooper@gmail.com>
2026-06-19 09:25:06 +0100

    Updated descriptions

--- test/pages.test.js
+++ test/pages.test.js
@@ -5,18 +5,24 @@ const assert = require("node:assert/strict");
 const fs = require("fs");
 const path = require("path");
 
-const STATIC_PAGES_FILE = path.resolve(__dirname, "../src/database/site/pages_static.json");
+const STATIC_PAGES_FILE = path.resolve(
+  __dirname,
+  "../src/database/site/pages_static.json",
+);
 const BUILT_PAGES_FILE = path.resolve(__dirname, "../.build/site/pages.json");
 
 test("pages.json has an entry for every page in pages_static.json", () => {
-  const { pages: staticPages } = JSON.parse(fs.readFileSync(STATIC_PAGES_FILE, "utf8"));
-  const { pages: builtPages } = JSON.parse(fs.readFileSync(BUILT_PAGES_FILE, "utf8"));
+  const { pages: staticPages } = JSON.parse(
+    fs.readFileSync(STATIC_PAGES_FILE, "utf8"),
+  );
+  const { pages: builtPages } = JSON.parse(
+    fs.readFileSync(BUILT_PAGES_FILE, "utf8"),
+  );
 
-  assert.equal(1,1);
-  /*assert.equal(
+  //assert.equal(1, 1);
+  assert.equal(
     builtPages.length,
     staticPages.length,
-    `expected ${staticPages.length} pages from pages_static.json, but .build/site/pages.json has ${builtPages.length}`
-  );*/
-
+    `expected ${staticPages.length} pages from pages_static.json, but .build/site/pages.json has ${builtPages.length}`,
+  );
 });
