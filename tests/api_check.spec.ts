c5f16e35be81dfa83d4217fbad03c8fe2b2dfe85
BP Hoops <benjaminpaulhooper@gmail.com>
2026-07-02 14:27:09 +0100

    api weather

--- tests/api_check.spec.ts
+++ tests/api_check.spec.ts
@@ -5,11 +5,17 @@ import { APIForcast } from "./weatherAPI/APIForcast";
 const latitude = 51.459563;
 const longitude = -2.790968;
 
-test("@API load forcast", async ({ page }) => {
+test("@API load current weather forcast", async ({ page }) => {
   const apiContext = await request.newContext();
   const apiForcast = new APIForcast(apiContext);
-  const response = await apiForcast.getForcast(latitude, longitude);
+  const response = await apiForcast.getWeatherCurrent(latitude, longitude);  
 
   const windspeed_units = response.current_weather_units.windspeed;
   expect(windspeed_units).toEqual("mp/h");
+});
+
+test("@API load forcast weather forcast", async ({ page }) => {
+  const apiContext = await request.newContext();
+  const apiForcast = new APIForcast(apiContext);
+  const response = await apiForcast.getWeatherForecast(latitude, longitude);  
 });
--- tests/weatherAPI/APIForcast.js
+++ tests/weatherAPI/APIForcast.js
@@ -1,23 +1,40 @@
-import { getCurrentWeatherUrl } from "../../src/js/framework/APIWeather";
+import {
+  getCurrentWeatherUrl,
+  getForcastWeatherUrl,
+} from "../../src/js/framework/APIWeather";
+import { test, expect, request } from "@playwright/test";
 
 export class APIForcast {
   constructor(apiContext) {
     this.apiContext = apiContext;
   }
 
-  async getForcast(latitude, longitude) {
-    const forcast_url = getCurrentWeatherUrl(latitude, longitude); //`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&wind_speed_unit=mph`;
+  async getWeatherCurrent(latitude, longitude) {
+    const forcast_url = getCurrentWeatherUrl(latitude, longitude);
 
     const forcastResponse = await this.apiContext.get(forcast_url, {
       headers: {
         "Content-Type": "application/json",
       },
     });
+    expect(forcastResponse.ok()).toBeTruthy();
+    const response = await forcastResponse.json();
+    console.log(response);
+    return response;
+  }
 
-    const responseOrder = await forcastResponse.json();
-    expect(responseOrder.ok()).toBeTruthy();
-    console.log(responseOrder);
-    return responseOrder;
+  async getWeatherForecast(latitude, longitude) {
+    const forcast_url = getForcastWeatherUrl(latitude, longitude);
+
+    const forcastResponse = await this.apiContext.get(forcast_url, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    expect(forcastResponse.ok()).toBeTruthy();
+    const response = await forcastResponse.json();
+    console.log(response);
+    return response;
   }
 }
 module.exports = { APIForcast };
